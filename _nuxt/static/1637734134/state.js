fa491f00b19077d74914256b9c9d655bbeea4b71
Heeming <Heeming@users.noreply.github.com>
2021-11-24 06:09:02 +0000

    deploy: 051b2188c670aa6b87487a8de2aab637f80d8d15

---
+++ _nuxt/static/1637734134/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1637734134",layout:"default",error:a,serverRendered:true,routePath:b,config:{googleAnalytics:{id:void 0},_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e5681a18"}}}}(null,"\u002F"));
